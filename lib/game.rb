9578666afe81e2ccf87fbb3cdd28b071c48c3f55
Annie Truex <artruex@gmail.com>
2019-08-27 23:59:33 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -11,8 +11,10 @@ class Game
     [6,4,2]
   ]
   
-  def initialize 
-    
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
   end
   
 end
